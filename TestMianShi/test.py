ba5f804fb6402fb166dda5ba69b642577bac4bc5
wangmj <397729921@qq.com>
2018-11-12 12:20:53 +0800

    requests

--- TestMianShi/test.py
+++ TestMianShi/test.py
@@ -1,12 +1,19 @@
-l = [1,2,3,2,1]
-for i in l:
-    print("this %s has found %s"%(i,l.count(i)))
-print('split line ------------------------')
+#删除重复元素
+listA = ['python','语','言','是','一','门','动','态','语','言']
+for i in set(listA):
+    print(i)
 
-s = set(l)
-for j in s:
-    print("this %s has fount %s" %(j,l.count(j)))
+import collections
+l =[item for item ,count in  collections.Counter(listA).items() if count == 1]
+print(l)
 
+#python 字典排序
+dic = {'a':3 , 'b':2 , 'c': 1,'d':3,'e':5}
+d =sorted(dic.items(),key=lambda asd:asd[0])
+print(d)
+
+
+#python如何删除dict中重复value值的item https://segmentfault.com/q/1010000000123481
 a = {
     1: 'a',
     2: 'a',
@@ -18,7 +25,5 @@ a = {
 }
 b = {a[key]:key for key in a}
 a = {b[key]:key for key in b}
+
 print(a)
-func = lambda a:dict([(x, y) for y, x in a.items()])
-func = lambda a:dict([(x ,y) for y ,x in a.items()])
-print(func(func(a)))
