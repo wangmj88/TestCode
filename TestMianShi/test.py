cca0a674e0f7da6654698833da1dafc37314faee
wangmj <397729921@qq.com>
2018-11-12 11:48:07 +0800

    requests

--- TestMianShi/test.py
+++ TestMianShi/test.py
@@ -6,3 +6,19 @@ print('split line ------------------------')
 s = set(l)
 for j in s:
     print("this %s has fount %s" %(j,l.count(j)))
+
+a = {
+    1: 'a',
+    2: 'a',
+    3: 'a',
+    4: 'b',
+    5: 'c',
+    6: 'c',
+    7: 'd'
+}
+b = {a[key]:key for key in a}
+a = {b[key]:key for key in b}
+print(a)
+func = lambda a:dict([(x, y) for y, x in a.items()])
+func = lambda a:dict([(x ,y) for y ,x in a.items()])
+print(func(func(a)))
